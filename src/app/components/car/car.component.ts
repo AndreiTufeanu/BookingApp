9bf81c0a6fb64dca74ce7b62785a571c3e628fc8
Andrei Tufeanu <andrei.tufeanu13@gmail.com>
2024-12-31 19:31:02 +0200

    feat: populate car and customer tables with data and ensure CRUD actions work

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,11 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Car } from '../../model/classes/Car';
+import { APIResponseModel } from '../../model/interfaces/APIResponseModel';
+import { CarService } from '../../services/car.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-car',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './car.component.html',
   styleUrl: './car.component.css'
 })
 export class CarComponent {
+  carObj: Car = new Car();
+  carList: Car[] = [];
 
+  carService = inject(CarService);
+
+  ngOnInit(): void {
+    this.loadAllCars();
+  }
+
+  loadAllCars() {
+    this.carService.getAllCars().subscribe((res:APIResponseModel)=> {
+      this.carList = res.data;
+    })
+  }
+
+  onSaveCar() {
+    this.carService.addCar(this.carObj).subscribe((res:APIResponseModel)=> {
+      if (res.result) {
+        alert("Car added successfully");
+        this.loadAllCars();
+        this.carObj = new Car();
+      }
+      else {
+        alert(res.message);
+      }
+    })
+  }
+
+  onUpdateCar() {
+    this.carService.updateCarInfo(this.carObj).subscribe((res:APIResponseModel)=> {
+      if (res.result) {
+        alert("Car updated successfully");
+        this.loadAllCars();
+        this.carObj = new Car();
+      }
+      else {
+        alert(res.message);
+      }
+    })
+  }
+
+  onEditCar(data: Car) {
+    this.carObj = data;
+  }
+
+  onDeleteCar(id: number) {
+    this.carService.deleteCar(id).subscribe((res:APIResponseModel)=> {
+      if (res.result) {
+        alert("Car deleted successfully");
+        this.loadAllCars();
+      }
+      else {
+        alert(res.message);
+      }
+    })
+  }
+
+  onReset() {
+    this.carObj = new Car();
+  }
 }
--- src/app/components/customer/customer.component.ts
+++ src/app/components/customer/customer.component.ts
@@ -1,11 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Customer } from '../../model/classes/Customer';
+import { CustomerService } from '../../services/customer.service';
+import { APIResponseModel } from '../../model/interfaces/APIResponseModel';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-customer',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css'
 })
 export class CustomerComponent {
+  customerObj: Customer = new Customer();
+  customersList: Customer[] = [];
 
+  customerService = inject(CustomerService);
+
+  ngOnInit(): void {
+    this.loadAllCustomers();
+  }
+
+  loadAllCustomers() {
+    this.customerService.getAllCustomers().subscribe((res: APIResponseModel) => {
+      this.customersList = res.data;
+    })
+  }
+
+  onSaveCustomer() {
+    this.customerService.addCustomer(this.customerObj).subscribe((res: APIResponseModel) => {
+      if (res.result) {
+        alert("Customer added successfully");
+        this.loadAllCustomers();
+        this.customerObj = new Customer();
+      }
+      else {
+        alert(res.message);
+      }
+    })
+  }
+
+  onUpdateCustomer() {
+    this.customerService.updateCustomerInfo(this.customerObj).subscribe((res: APIResponseModel) => {
+      if (res.result) {
+        alert("Customer updated successfully");
+        this.loadAllCustomers();
+        this.customerObj = new Customer();
+      }
+      else {
+        alert(res.message);
+      }
+    })
+  }
+
+  onEditCustomer(data: Customer) {
+    this.customerObj = data;
+  }
+
+  onDeleteCustomer(id: number) {
+    this.customerService.deleteCustomer(id).subscribe((res: APIResponseModel) => {
+      if (res.result) {
+        alert("Customer deleted successfully");
+        this.loadAllCustomers();
+      }
+      else {
+        alert(res.message);
+      }
+    })
+  }
+
+  onReset() {
+    this.customerObj = new Customer();
+  }
 }
