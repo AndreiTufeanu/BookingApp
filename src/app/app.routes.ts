20fc405c1336af69423b33e7c577bb3d41d51cd2
Andrei Tufeanu <andrei.tufeanu13@gmail.com>
2024-12-31 15:16:12 +0200

    feat: add routing and navbar for Car, Customer, and Booking components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { CarComponent } from './components/car/car.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { BookingComponent } from './components/booking/booking.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'car',
+        pathMatch: 'full'
+    },
+    {
+        path: 'car',
+        component: CarComponent
+    },
+    {
+        path: 'customer',
+        component: CustomerComponent
+    },
+    {
+        path: 'booking',
+        component: BookingComponent
+    }
+];
